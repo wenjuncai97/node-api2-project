23037e88c3840152e58bc5bac5e171a480b32af9
Nathan Cai <wenjuncai97@gmail.com>
2022-05-11 02:39:27 -0400

    finished post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -32,11 +32,31 @@ router.get('/:id', async (req, res) => {
             err: err.message,
             stack: err.stack,
         })
-    } 
+    }
 })
 
 router.post('/', (req, res) => {
-
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+        })
+    } else {
+        Post.insert({ title, contents })
+            .then(({id}) => {
+                return Post.findById(id);
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 })
 
 router.delete('/:id', (req, res) => {
